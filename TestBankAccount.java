45dadecf8129c06e421cfdbbcae6fa0ad54ed6b0
Ubuntu <ubuntu@ip-172-31-14-24.us-west-1.compute.internal>
2015-02-16 12:37:00 +0000

    first commit

---
+++ TestBankAccount.java
@@ -0,0 +1,16 @@
+package ca.uwo.csd.cs2212.USERNAME;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBankAccount {
+
+	@Test
+	public void testDebitWithSufficientFund() {
+		BankAccount acc = new BankAccount(10);
+		double amount = acc.debit(5);
+		Assert.assertEquals(5.0, amount);
+	}
+
+}
